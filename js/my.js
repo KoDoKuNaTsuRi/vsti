975fc3973cb66dbb30e208319b7092ec350d223e
KoDoKuNaTsuRi <3046162104@qq.com>
2024-07-28 22:24:28 +0800

    Site updated: 2024-07-28 22:24:27

---
+++ js/my.js
@@ -0,0 +1,4 @@
+var sidebarmenus = document.getElementById("sidebar-menus");
+sidebarmenus.removeChild(sidebarmenus.childNodes[0]);
+sidebarmenus.removeChild(sidebarmenus.childNodes[0]);
+sidebarmenus.removeChild(sidebarmenus.childNodes[0]);
